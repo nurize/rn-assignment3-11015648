e2e42814948ee3e18b6eea54fa133152f42c8b50
Eunice Dickson <sunshinebae439@gmail.com>
2024-05-31 18:52:10 +0000

    Created the app header

--- Docket/Docket/App.js
+++ Docket/Docket/App.js
@@ -1,18 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
+const headerImage = require('./assets/images/header_image.png')
 export default function App() {
   return (
     <View style={styles.container}>
       <View  style={styles.header}>
         <View style={styles.headerText}>
-        <Text style={styles.hello}>Hello, Devs</Text>
-        <Text>14 tasks today</Text>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>Hello, Devs</Text>
+          <Text>14 tasks today</Text>
         </View>
         <View>
-
+          <Image style={{width: 55, height: 55, backgroundColor: 'white', borderRadius: 50}} source={headerImage}/>
         </View>
       </View>
+      
     </View>
   );
 }
@@ -26,5 +27,15 @@ const styles = StyleSheet.create({
   },
   header: {
     backgroundColor: 'transparent',
+    width: 354,
+    height: 52,
+    margin: {
+      marginTop: 52,
+      marginRight: 0,
+      marginBottom: 50,
+      marginLeft: 20,
+    },
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
